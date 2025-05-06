871611d8a1eede07382ca654467bdc43f9c51d0d
hexiaofeng <hexiaofeng@jd.com>
2025-05-06 15:03:52 +0800

    Fix dubbo 2.7 demo

--- joylive-demo-dubbo2.7/joylive-demo-dubbo2.7-provider/src/main/java/com/jd/live/agent/demo/dubbo/v2_7/provider/service/Dubbo27Service.java
+++ joylive-demo-dubbo2.7/joylive-demo-dubbo2.7-provider/src/main/java/com/jd/live/agent/demo/dubbo/v2_7/provider/service/Dubbo27Service.java
@@ -15,32 +15,75 @@
  */
 package com.jd.live.agent.demo.dubbo.v2_7.provider.service;
 
+import com.jd.live.agent.demo.dubbo.v2_7.provider.config.EchoConfig;
+import com.jd.live.agent.demo.exception.BreakableException;
+import com.jd.live.agent.demo.exception.RetryableException;
 import com.jd.live.agent.demo.response.LiveLocation;
 import com.jd.live.agent.demo.response.LiveResponse;
 import com.jd.live.agent.demo.response.LiveTrace;
 import com.jd.live.agent.demo.response.LiveTransmission;
 import com.jd.live.agent.demo.service.HelloService;
+import com.jd.live.agent.demo.service.SleepService;
+import com.jd.live.agent.demo.util.CpuBusyUtil;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.beans.factory.annotation.Value;
 
-@DubboService(group = "live-demo", interfaceClass = HelloService.class)
-public class Dubbo27Service implements HelloService {
+import java.util.concurrent.ThreadLocalRandom;
 
-    @Value("${spring.application.name}")
-    private String applicationName;
+@DubboService(group = "DEFAULT_GROUP", interfaceClass = HelloService.class)
+public class Dubbo27Service implements SleepService {
+
+    private final String applicationName;
+
+    private final EchoConfig config;
+
+    @Value("${echo.suffix}")
+    private String echoSuffix;
+
+    @Value("${mock.cpuPercent:0.2}")
+    private double cpuPercent;
+
+    public Dubbo27Service(@Value("${spring.application.name}") String applicationName, EchoConfig config) {
+        this.applicationName = applicationName;
+        this.config = config;
+    }
 
     @Override
     public LiveResponse echo(String str) {
-        return createResponse(str);
+        int sleepTime = config.getSleepTime();
+        if (sleepTime > 0) {
+            if (config.getRandomTime() > 0) {
+                sleepTime = sleepTime + ThreadLocalRandom.current().nextInt(config.getRandomTime());
+            }
+            CpuBusyUtil.busyCompute(sleepTime);
+        }
+        String value = str + "-sleepTime-" + config.getSleepTime() + "-cpuPercent-" + cpuPercent;
+        String suffix = config.getSuffix();
+        if (suffix != null && !suffix.isEmpty()) {
+            value = value + "-" + suffix;
+        }
+        return createResponse(value);
     }
 
     @Override
     public LiveResponse status(int code) {
-        if (code >= 500) {
-            throw new RuntimeException("Code:" + code);
+        if (code == 600) {
+            if (ThreadLocalRandom.current().nextInt(2) == 0) {
+                throw new RetryableException("Code:" + code);
+            }
+        } else if (code >= 500) {
+            throw new BreakableException("Code:" + code);
         }
         return createResponse(code);
+    }
+
+    @Override
+    public LiveResponse sleep(int millis) {
+        if (millis > 0) {
+            CpuBusyUtil.busyCompute(millis);
+        }
+        return createResponse(null);
     }
 
     private LiveResponse createResponse(Object data) {
