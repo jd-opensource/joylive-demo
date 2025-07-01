61acedadbc8e6be6fa98bf0638800c05b31ccb72
hexiaofeng <hexiaofeng@jd.com>
2025-07-01 11:55:50 +0800

    Update jedis4&5

---
+++ joylive-demo-springcloud2022/joylive-demo-springcloud2022-order-redis/src/main/java/com/jd/live/agent/demo/springcloud/v2022/order/servcice/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.demo.springcloud.v2022.order.servcice.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jd.live.agent.demo.springcloud.v2022.order.entity.User;
+import com.jd.live.agent.demo.springcloud.v2022.order.mapper.UserMapper;
+import com.jd.live.agent.demo.springcloud.v2022.order.servcice.UserService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    @Cacheable(value = "user", key = "#id")
+    @Override
+    public User getById(Serializable id) {
+        return super.getById(id);
+    }
+
+    @Cacheable(value = "user", key = "#code")
+    @Override
+    public User getByCode(String code) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("code", code);
+        return baseMapper.selectOne(queryWrapper);
+    }
+}
