919f499e711388ad880c1e15838ad8bd74a20ddf
hexiaofeng <hexiaofeng@jd.com>
2025-08-22 14:23:20 +0800

    Add demo

--- joylive-demo-springcloud2021/joylive-demo-springcloud2021-consumer-war/src/main/java/com/jd/live/agent/demo/springcloud/v2021/consumer/SpringCloud2021WarConsumer.java
+++ joylive-demo-springcloud2021/joylive-demo-springcloud2021-consumer-war/src/main/java/com/jd/live/agent/demo/springcloud/v2021/consumer/SpringCloud2021WarConsumer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jd.live.agent.demo.springcloud.v2021.provider;
+package com.jd.live.agent.demo.springcloud.v2021.consumer;
 
 import com.alibaba.nacos.client.config.utils.SnapShotSwitch;
 import org.springframework.boot.SpringApplication;
@@ -21,28 +21,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class SpringCloud2021Provider extends SpringBootServletInitializer {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SpringCloud2021WarConsumer extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SnapShotSwitch.setIsSnapShot(false);
-        SpringApplication.run(SpringCloud2021Provider.class, args);
+        SpringApplication.run(SpringCloud2021WarConsumer.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringCloud2021Provider.class);
+        return builder.sources(SpringCloud2021WarConsumer.class);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     Object index() {
-        return "<h1>service-provider-2021</h1>";
+        return "<h1>service-consumer-2021</h1>";
     }
 
 }
